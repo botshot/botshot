370e0766896d9814083ac0e51930ea91487945a9
Matus Zilinec <zilinec.m@gmail.com>
2019-01-17 18:48:46 +0100

    Add pytest to requirements.

--- setup.py
+++ setup.py
@@ -42,5 +42,6 @@ setup(
         'Topic :: Communications :: Chat',
     ],
     install_requires=['django>=2.0.0', 'networkx', 'requests', 'six', 'sqlparse', 'wit==4.3.0', 'wheel', 'redis', 'Pillow', 'jsonfield',
-                      'pytz', 'unidecode', 'emoji', 'elasticsearch', 'celery>=4.1.1', 'python-dateutil', 'pyyaml', 'djangorestframework'],
+                      'pytz', 'unidecode', 'emoji', 'elasticsearch', 'celery>=4.1.1', 'python-dateutil', 'pyyaml', 'djangorestframework',
+                      'pytest', 'pytest-django', 'mock'],
 )
